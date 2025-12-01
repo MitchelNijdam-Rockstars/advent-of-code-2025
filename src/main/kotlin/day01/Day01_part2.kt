bb476bab304d19f68d65f6afa7b11adb3cee73b0
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2025-12-01 20:32:09 +0100

    day 1 setup

---
+++ src/main/kotlin/day01/Day01_part2.kt
@@ -0,0 +1,26 @@
+package day01
+
+import printOutput
+import printTestOutput
+import readInput
+import readTestInput
+
+fun main() {
+    val day = 1
+    val part = 2
+    println("\nExecuting part $part of day $day\n\n")
+
+    fun solvePuzzle(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readTestInput(day)
+    val testSolution = solvePuzzle(testInput)
+
+    printTestOutput(testInput, testSolution)
+
+//    val input = readInput(day)
+//    val solution = solvePuzzle(input)
+//
+//    printOutput(solution)
+}
