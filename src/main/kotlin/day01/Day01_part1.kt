75206635521c10c7b593156670653582718307a8
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2025-12-01 23:18:05 +0100

    day 1 part 1

--- src/main/kotlin/day01/Day01_part1.kt
+++ src/main/kotlin/day01/Day01_part1.kt
@@ -4,23 +4,55 @@ import printOutput
 import printTestOutput
 import readInput
 import readTestInput
+import kotlin.math.absoluteValue
 
 fun main() {
     val day = 1
     val part = 1
     println("\nExecuting part $part of day $day\n\n")
 
-    fun solvePuzzle(input: List<String>): Int {
-        return input.size
-    }
+
 
     val testInput = readTestInput(day)
-    val testSolution = solvePuzzle(testInput)
+    val testSolution = solvePuzzlePart1(testInput)
 
     printTestOutput(testInput, testSolution)
 
-//    val input = readInput(day)
-//    val solution = solvePuzzle(input)
-//
-//    printOutput(solution)
+    val input = readInput(day)
+    val solution = solvePuzzlePart1(input)
+
+    printOutput(solution)
+}
+
+fun solvePuzzlePart1(input: List<String>): Int {
+    val dialNumbers = (0..99).toList().toIntArray()
+    val startPoint = 50
+    var currentPoint = startPoint
+    var visitedZero = 0
+    for (directions in input) {
+        val direction = directions.first()
+        val distance = directions.drop(1).toInt()
+
+        if (direction == 'R') {
+            currentPoint = if (currentPoint + distance > dialNumbers.size - 1) {
+                (currentPoint + distance) % dialNumbers.size
+            } else {
+                (currentPoint + distance)
+            }
+        }
+        if (direction == 'L') {
+            currentPoint = if(currentPoint - distance < 0) {
+                val temp = (dialNumbers.size + ((currentPoint - distance) % dialNumbers.size))
+                if(temp == dialNumbers.size) 0 else temp
+            } else {
+                currentPoint - distance
+            }
+        }
+
+        println(currentPoint)
+
+        if (currentPoint == 0) visitedZero++
+    }
+
+    return visitedZero
 }
