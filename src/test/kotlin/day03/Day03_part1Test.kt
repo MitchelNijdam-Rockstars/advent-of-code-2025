ae00f1223ca28031e5afa1a727ac0205285937eb
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2025-12-03 18:38:01 +0100

    day 3 setup

---
+++ src/test/kotlin/day03/Day03_part1Test.kt
@@ -0,0 +1,35 @@
+package day03
+
+import day02.solvePuzzlePart1
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day03_part1Test {
+
+    @Test
+    fun test1() {
+        val input = listOf(
+            ""
+        )
+        val result = solvePuzzlePart1(input)
+
+        assertEquals(1L, result)
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(
+        "11,2",
+        "32,4"
+    )
+    fun test1(input: String, expectedOutput: Long) {
+        val inputList = listOf(input)
+
+        val result = solvePuzzlePart1(inputList)
+
+        assertEquals(expectedOutput, result)
+    }
+
+}
---
+++ src/test/kotlin/day03/Day03_part2Test.kt
@@ -0,0 +1,33 @@
+package day03
+
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day03_part2Test {
+
+    @Test
+    fun test1() {
+        val input = listOf(
+            ""
+        )
+        val result = solvePuzzlePart2(input)
+
+        assertEquals(1L, result)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "11,2",
+        "32,4"
+    )
+    fun test1(input: String, expectedOutput: Long) {
+        val inputList = listOf(input)
+
+        val result = solvePuzzlePart2(inputList)
+
+        assertEquals(expectedOutput, result)
+    }
+
+}
